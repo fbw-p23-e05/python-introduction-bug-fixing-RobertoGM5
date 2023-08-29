3dcc1e704ca41820abd87bac321417ae5b360799
Roberto Galvez Meyer <139347219+RobertoGM5@users.noreply.github.com>
2023-08-29 14:29:58 +0200

    added sixth solution
    added Task6.py

---
+++ Task6.py
@@ -0,0 +1,10 @@
+x = int(input("First number: "))
+y = int(input("Second number: "))
+
+result = x + y
+
+if result > 15 and result < 20:
+    sum = 20
+    print("Calculated sum is ", sum)
+else:
+    print(result)
