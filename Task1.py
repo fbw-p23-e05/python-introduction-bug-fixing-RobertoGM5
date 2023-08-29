1fd82940f75e2c634fe078a9254e4cc7cbdc57eb
Roberto Galvez Meyer <139347219+RobertoGM5@users.noreply.github.com>
2023-08-29 14:57:55 +0200

    corrected Task1
    updated Task1.py

--- Task1.py
+++ Task1.py
@@ -1,14 +1,17 @@
 three_mul = 'fizz'
 five_mul = 'buzz'
+both = 'fizzbuzz'
+
 num1 = 3
-num2 = 4
+num2 = 5
 max_num = 100
 for i in range(1,max_num):
     # % or modulo division gives you the remainder
     
-    if i%num1 == 0:
+    if i%num1 == 0 and i%num2 == 0:
+        print(i, both)
+    elif i%num1 == 0:
         print(i, three_mul)
     elif i%num2 == 0:
         print(i, five_mul)
-    elif i%num1 == 0 and i%num2 == 0:
-        print(i, three_mul+five_mul)
+    
