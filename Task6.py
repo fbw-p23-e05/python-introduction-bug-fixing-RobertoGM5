5cb865663b84014016b0d15435bc3c630e3bc702
Roberto Galvez Meyer <139347219+RobertoGM5@users.noreply.github.com>
2023-08-30 10:32:42 +0200

    Add files via upload

--- Task6.py
+++ Task6.py
@@ -4,7 +4,5 @@ y = int(input("Second number: "))
 result = x + y
 
 if result >= 15 and result <= 20:
-    sum = 20
-    print("Calculated sum is ", sum)
-else:
-    print(result)
+    result = 20
+print("Calculated sum is ", result)
