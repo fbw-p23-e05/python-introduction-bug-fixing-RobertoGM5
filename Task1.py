ddd0f9e9aec02e9ea81f46e08b24012bf58e3000
Roberto Galvez Meyer <139347219+RobertoGM5@users.noreply.github.com>
2023-08-30 09:27:37 +0200

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,6 +1,5 @@
 three_mul = 'fizz'
 five_mul = 'buzz'
-both = 'fizzbuzz'
 
 num1 = 3
 num2 = 5
@@ -9,7 +8,7 @@ for i in range(1,max_num):
     # % or modulo division gives you the remainder
     
     if i%num1 == 0 and i%num2 == 0:
-        print(i, both)
+        print(i, three_mul+five_mul)
     elif i%num1 == 0:
         print(i, three_mul)
     elif i%num2 == 0:
